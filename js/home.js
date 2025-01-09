165182be9f51babc86ae0443f0b5177d0dca001f
SamuelFDEZS <thggsamuel@gmail.com>
2025-01-09 01:34:10 +0100

    Develop login logic and create home js file

---
+++ js/home.js
@@ -0,0 +1,6 @@
+/* TODO */
+
+/**Es necesario añadir lógica para comprobar si el usuario está logueado o no en todas las páginas, en el caso del home, al estar logueado hay que borrar los botones de sign up y login y poner el nombre del usuario (nickname), el cual se encuentra en localStorage o bien en sessionStorage, Hará falta comprobar en ambos sitios:
+ *
+ * if(localStorage.getItem('currentUser') || sessionStorage.getItem('currentUser)){...}
+*/
