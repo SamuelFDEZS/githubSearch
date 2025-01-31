3f144893a20741ca0122d6846d2c23ff7118592b
SamuelFDEZS <102028573+SamuelFDEZS@users.noreply.github.com>
2025-01-31 14:57:13 +0100

    Create user search component and create logic (#13)

--- js/home.js
+++ js/home.js
@@ -1,33 +1,11 @@
 const buttonContainer = document.querySelector('.header__btn-container');
 const welcomeLoginBtn = document.querySelector('.welcome__login-btn');
-const currentUser = JSON.parse(localStorage.getItem('currentUser') || sessionStorage.getItem('currentUser'));
+const isUserLogged = JSON.parse(localStorage.getItem('currentUser') || sessionStorage.getItem('currentUser'));
 
-const userContainer = document.querySelector('.header__user-container');
-const nickContainer = document.querySelector('.header__user-container__nickname');
-const userIcon = document.querySelector('.header__user-container__user-icon');
-const userArrow = document.querySelector('.header__user-container__arrow');
-const userMenu = document.querySelector('.header__user-container__user-menu');
-
-if(currentUser) {
+if(isUserLogged) {
     buttonContainer.classList.add('login__element__hidden');
     welcomeLoginBtn.classList.add('login__element__hidden');
-
-    userIcon.classList.remove('login__element__hidden');
-    nickContainer.classList.remove('login__element__hidden');
-    nickContainer.innerHTML = currentUser.nickname;
 } else {
-    userIcon.classList.add('login__element__hidden');
-    userContainer.classList.add('login__element__hidden');
     buttonContainer.classList.remove('login__element__hidden');
     welcomeLoginBtn.classList.remove('login__element__hidden');
 }
-
-
-const handleUserClick = () => {
-    userArrow.classList.toggle('fa-angle-rotated');
-    userContainer.classList.toggle('header__user-container__active-background');
-
-    userMenu.classList.toggle('visible')
-}
-
-userContainer.addEventListener('click', handleUserClick);
